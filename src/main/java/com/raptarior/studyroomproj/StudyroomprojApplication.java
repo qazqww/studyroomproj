ee8ce8fa5c2272d444641faa7970abb1f2a4f76d
raptarior <qazqww@naver.com>
2025-09-19 16:27:26 +0900

    first commit

---
+++ src/main/java/com/raptarior/studyroomproj/StudyroomprojApplication.java
@@ -0,0 +1,13 @@
+package com.raptarior.studyroomproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyroomprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyroomprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raptarior/studyroomproj/StudyroomprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raptarior.studyroomproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyroomprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
